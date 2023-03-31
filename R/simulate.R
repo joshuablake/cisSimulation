d884a862e35f595e0dcdd66266db1b922955823a
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2023-03-31 14:44:47 +0100

    Initial functionality

---
+++ R/simulate.R
@@ -0,0 +1,128 @@
+#' Simulate the CIS study
+#'
+#' @param positive_duration A duration function specifying the duration distribution to use
+#' @param n_indivs The number of individuals (episodes) to simulate
+#' @param testing_times A testing function specifying the times at which individuals are tested
+#' @param infection_times An infections function specifying the shape of the epidemic
+#' @param testing A sensitivity or other function specifying how to simulate the testing results
+#' @import dplyr
+#' @importFrom tibble tibble
+#' @importFrom rlang .data
+#' @export
+simulate_cis = function(
+    positive_duration,
+    n_indivs = 1e6,
+    testing_times = testing_fixed(),
+    infection_times = infections_flat(),
+    testing = sensitivity_fixed()
+    ) {
+
+  tbl_individuals = tibble(
+    i = 1:n_indivs
+  ) |>
+    mutate(
+      infection_time = infection_times(n()),
+      duration = positive_duration(n())
+    )
+
+  testing_times(n_indivs) |>
+    left_join(tbl_individuals(by = "i")) |>
+    mutate(test_result = testing(.data[["test_time"]] - .data[["infection_time"]], .data[["duration"]]))
+}
+
+#' Fixed interval testing times
+#'
+#' @param test_interval Time between tests.
+#' @param first_test First possible testing time. Each individual will be tested first at a time between `first_test` and `first_test + test_interval - 1` chosen uniformly at random (this gives an even spread of testing times).
+#' @param last_test Last possible testing time.
+#' @import dplyr
+#' @import tidyr
+#' @importFrom tibble tibble
+#' @importFrom rlang .data
+#' @export
+testing_fixed = function(test_interval = 7, first_test = -90, last_test = 300) {
+  tests_after_first = seq.int(from = 0, to = last_test - first_test, by = test_interval)
+  function(n_indivs) {
+    tibble(
+      i = 1:n_indivs
+    ) |>
+      mutate(
+        tests_after_first = list(tests_after_first),
+        i_first_test = sample.int(test_interval, n(), TRUE) - 1 + first_test,
+      ) |>
+      unnest("tests_after_first") |>
+      transmute(.data[["i"]], test_time = .data[["i_first_test"]] + .data[["tests_after_first"]]) |>
+      filter(between(.data[["test_time"]], first_test, last_test))
+  }
+}
+
+#' Custom testing times
+#'
+#' @param tbl_times Tibble with two columns i (individual) and test_time for each test
+#' @export
+testing_custom = function(tbl_times) {
+  schedule_ids = unique(tbl_times$i)
+  function(n_indivs) {
+    tibble::tibble(
+      i = 1:n_indivs
+    ) |>
+      dplyr::mutate(
+        test_schedule = sample(schedule_ids, n(), replace = TRUE)
+      ) |>
+      dplyr::left_join(schedule_ids, by = c("test_schedule" = "i"))
+  }
+}
+
+#' Specify a custom distribution for the duration of positivity
+#'
+#' @param pmf Probability mass function (vector) for the distribution (automatically normalised), assumed to have support 1:length(pmf)
+#' @export
+duration_custom = function(pmf) {
+  function(n) {
+    sample.int(length(pmf), n, replace = TRUE, prob = pmf)
+  }
+}
+
+#' A flat epidemic
+#' @export
+infections_flat = function () {
+  function(n) rep(1, n)
+}
+
+#' A fixed test sensitivity with no false positives
+#'
+#' @param sensitivity The test sensitivity to use, the default is no false negatives
+#' @importFrom stats runif
+#' @export
+sensitivity_fixed = function(sensitivity = 1) {
+  function(time_since_infection, duration) {
+    stopifnot(length(time_since_infection) == length(duration))
+    n = length(time_since_infection)
+    (time_since_infection >= 0) & (time_since_infection < duration) & (runif(n) < sensitivity)
+  }
+}
+
+#' A test sensitivity that decreases linearly over time to a constant value
+#'
+#' @param min,max Minimum and maximum test sensitivity to use
+#' @param time_of_min Days after infection when minimum occurs
+#' @importFrom stats approx
+#' @importFrom stats runif
+#' @export
+sensitivity_linear = function(max = 0.9, min = 0.5, time_of_min = 50) {
+  stopifnot(max <= 1)
+  stopifnot(min >= 0)
+  stopifnot(max >= min)
+  stopifnot(time_of_min >= 1)
+  function(time_since_infection, duration) {
+    stopifnot(length(time_since_infection) == length(duration))
+    n = length(time_since_infection)
+    sensitivity = approx(
+      x = c(0, time_of_min),
+      y = c(max, min),
+      xout = time_since_infection,
+      rule = 2
+    )$y
+    (time_since_infection >= 0) & (time_since_infection < duration) & (runif(n) < sensitivity)
+  }
+}
---
+++ tests/testthat/test-simulate.R
@@ -0,0 +1,100 @@
+test_that("daily tests", {
+  expected_len = 15
+  result = testing_fixed(1, 0, 14)(1)
+  expect_length(result$i, expected_len)
+  expect_equal(result$i, rep(1, expected_len))
+  expect_equal(result$test_time, 0:14)
+})
+
+test_that("weekly tests", {
+  result = testing_fixed(7, 0, 20)(10)
+  expect_true(all(table(result$i) == 3))
+  expect_equal(dplyr::n_distinct(result$i), 10)
+  expect_true(all(result$test_time >= 0))
+  expect_true(all(result$test_time <= 20))
+})
+
+test_that("tests are uniform", {
+  n_indiv = 100e3
+  expected_len = 10 * n_indiv
+  result = testing_fixed(10, 1, 100)(n_indiv)
+  expect_true(all(table(result$i) == 10))
+  expect_equal(dplyr::n_distinct(result$i), n_indiv)
+  expect_true(all(result$test_time >= 1))
+  expect_true(all(result$test_time <= 100))
+  tests_by_day = table(result$test_time)
+  expect_true(all(tests_by_day >= 9000))
+  expect_true(all(tests_by_day <= 11000))
+})
+
+test_that("test results duration", {
+  duration = 10
+  times = -15:30
+  input_len = length(times)
+  result = sensitivity_fixed(1)(times, rep(duration, input_len))
+  expect_true(all(result %in% c(0, 1)))
+  expect_equal(sum(result), duration)
+  expect_length(result, input_len)
+})
+
+test_that("test results sensitivity", {
+  sensitivity = 0.5
+  duration = 10
+  times = rep(0, 1e6)
+  input_len = length(times)
+  result = sensitivity_fixed(sensitivity)(times, rep(duration, input_len))
+  expect_true(all(result %in% c(0, 1)))
+  expect_length(result, input_len)
+  expect_lt(sum(result), 502e3)
+  expect_gt(sum(result), 498e3)
+})
+
+test_that("test results sensitivity and duration", {
+  sensitivity = 0.5
+  duration = 10
+  times = rep(-15:30, 1e5)
+  input_len = length(times)
+  result = sensitivity_fixed(sensitivity)(times, rep(duration, input_len))
+  expect_true(all(result %in% c(0, 1)))
+  expect_length(result, input_len)
+  expect_lt(sum(result), 502e3)
+  expect_gt(sum(result), 498e3)
+})
+
+test_that("test results sensitivity linear", {
+  n_indiv = 1e6
+  sensitivity = 0.5
+  duration = 7
+  uniq_times = 0:9
+  true_pos_proportion = c(1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.5, rep(0, 3))
+  stopifnot(length(true_pos_proportion) == length(uniq_times))
+  times = rep(uniq_times, n_indiv)
+  input_len = length(times)
+  result = sensitivity_linear(1, 0.5, 5)(times, rep(duration, input_len))
+  expect_true(all(result %in% c(0, 1)))
+  expect_length(result, input_len)
+
+  result_summary = tibble::tibble(
+    i = rep(1:n_indiv, length(uniq_times)),
+    time = times,
+    result = result,
+  ) |>
+    dplyr::group_by(time) |>
+    dplyr::summarise(n = n(), pos = sum(result), .groups = "drop") |>
+    dplyr::arrange(time)
+  expect_equal(result_summary$time, uniq_times)
+  expect_true(all(result_summary$n == n_indiv))
+  expect_true(all(result_summary$pos >= qbinom(1e-4, n_indiv, true_pos_proportion)))
+  expect_true(all(result_summary$pos <= qbinom(1 - 1e-4, n_indiv, true_pos_proportion)))
+})
+
+test_that("test flat infections", {
+  test_len = function(n) {
+    result = infections_flat()(n)
+    expect_length(result, n)
+    expect_true(all(result == 1))
+  }
+  test_len(10)
+  test_len(500)
+  test_len(1e6)
+})
